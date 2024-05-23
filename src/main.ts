dfaed84ecea938df0065e5c9903809268be85b81
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-05-24 03:19:09 +0800

    file download

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { chainSpec } from "polkadot-api/chains/polkadot_collectives";
 import SmWorker from "polkadot-api/smoldot/worker?worker";
 import { startFromWorker } from "polkadot-api/smoldot/from-worker";
 
+document.body.textContent = "Syncying...";
+
 // Running smoldot in a web-worker has many advantages: the bundler
 // will create a separate "chunk" with the smoldot binaries (which are very heavy),
 // and the compilation of smoldot won't block the main process...
@@ -34,8 +36,12 @@ const chain = relayChainPromise.then((relayChain) =>
 
 // Connect to the parachain
 const client = createClient(getSmProvider(chain));
-const finalizedBlock = await client.getFinalizedBlock();
 const fellowshipApi = client.getTypedApi(fellowship);
+
+const runtime = await fellowshipApi.runtime.latest();
+document.body.textContent = "Ready!";
+
+const finalizedBlock = await client.getFinalizedBlock();
 const finalizedTimestamp = await fellowshipApi.query.Timestamp.Now.getValue({
   at: finalizedBlock.hash,
 });
@@ -54,14 +60,15 @@ function blockToDate(block: number): Date {
 const calendar = ical({ name: "Fellowship Calendar" });
 
 // get the value for an account
-const salaryStatus =
-  await fellowshipApi.query.FellowshipSalary.Status.getValue();
+let currentCycleStart =
+  (await fellowshipApi.query.FellowshipSalary.Status.getValue())!.cycle_start;
 
-let currentCycleStart = salaryStatus!.cycle_start;
+document.body.textContent = "Calendar data received";
+
 const registrationPeriod =
-  await fellowshipApi.constants.FellowshipSalary.RegistrationPeriod();
+  fellowshipApi.constants.FellowshipSalary.RegistrationPeriod(runtime);
 const payoutPeriod =
-  await fellowshipApi.constants.FellowshipSalary.PayoutPeriod();
+  fellowshipApi.constants.FellowshipSalary.PayoutPeriod(runtime);
 
 for (let cycle = 0; cycle < 12; cycle++) {
   calendar.createEvent({
@@ -90,6 +97,8 @@ const [coreParams, memberStatus, memberRank] = await Promise.all([
 
 if (!memberRank || !memberStatus)
   throw new Error(`Member ${ADDRESS} not found`);
+
+document.body.textContent = "User data loaded";
 
 let timeout: number;
 let approachSummary: string;
@@ -126,4 +135,8 @@ calendar.createEvent({
   summary: happenedSummary,
 });
 
-document.body.textContent = calendar.toString();
+(globalThis as any).loadCal = () => {
+  window.open("data:text/calendar;charset=utf8," + calendar.toString());
+};
+
+document.body.innerHTML = `<a href="javascript:loadCal();">Open Calendar file</a>`;
