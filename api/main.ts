71f5cc0f9a72953529572ffc1868db213322046c
Bastian Köcher <info@kchr.de>
2024-06-16 10:52:55 +0200

    Make it work

--- api/main.ts
+++ api/main.ts
@@ -3,134 +3,137 @@ import { fellowship } from "@polkadot-api/descriptors";
 import { createClient } from "polkadot-api";
 import { getSmProvider } from "polkadot-api/sm-provider";
 import { chainSpec } from "polkadot-api/chains/polkadot_collectives";
-import SmWorker from "polkadot-api/smoldot/worker";
-import { startFromWorker } from "polkadot-api/smoldot/from-worker";
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { chainSpec as relayChainSpec } from "polkadot-api/chains/polkadot";
+import { start } from "polkadot-api/smoldot";
+import { timeStamp } from "console";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-
-// Running smoldot in a web-worker has many advantages: the bundler
-// will create a separate "chunk" with the smoldot binaries (which are very heavy),
-// and the compilation of smoldot won't block the main process...
-// ie: faster loading times, plus smoldot will have a dedicated process.
-const smoldot = startFromWorker(new SmWorker());
-
-// this will make the bundler create a separate chunk for the polkadot
-// chainSpec (which is quite heavy) and it will load the chain without
-// blocking the rest of the tasks that can be ran in parallel
-const relayChainPromise = import("polkadot-api/chains/polkadot").then(
-  ({ chainSpec }) =>
-    smoldot.addChain({
-      chainSpec,
-      disableJsonRpc: true,
-    }),
-);
-
-// likewise: there is no need to "await" in here because the `getSmProvider`
-// accepts a `Promise<Chain>` so that the loading can happen in the background
-const chain = relayChainPromise.then((relayChain) =>
-  smoldot.addChain({
+const smoldot = start();
+const relayChain = await smoldot.addChain({ chainSpec: relayChainSpec });
+const chain = await smoldot.addChain({
     chainSpec,
     potentialRelayChains: [relayChain],
-  }),
-);
-
+});
 // Connect to the parachain
 const client = createClient(getSmProvider(chain));
 const finalizedBlock = await client.getFinalizedBlock();
 const fellowshipApi = client.getTypedApi(fellowship);
 const finalizedTimestamp = await fellowshipApi.query.Timestamp.Now.getValue({
-  at: finalizedBlock.hash,
+    at: finalizedBlock.hash,
 });
 
 function blockToDate(block: number): Date {
-  const diff =
-    block >= finalizedBlock.number
-      ? block - finalizedBlock.number
-      : finalizedBlock.number - block;
-
-  return new Date(
-    new Date(Number(finalizedTimestamp) + diff * 12 * 1000).toDateString(),
-  );
+    if (block >= finalizedBlock.number) {
+        return new Date(
+            new Date(
+                Number(finalizedTimestamp) +
+                    (block - finalizedBlock.number) * 12 * 1000,
+            ).toDateString(),
+        );
+    } else {
+        return new Date(
+            new Date(
+                Number(finalizedTimestamp) -
+                    (finalizedBlock.number - block) * 12 * 1000,
+            ).toDateString(),
+        );
+    }
 }
 
-const calendar = ical({ name: "Fellowship Calendar" });
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    const { name = "World" } = req.query;
 
-// get the value for an account
-const salaryStatus =
-  await fellowshipApi.query.FellowshipSalary.Status.getValue();
+    const calendar = ical({ name: "Fellowship Calendar" });
 
-let currentCycleStart = salaryStatus!.cycle_start;
-const registrationPeriod =
-  await fellowshipApi.constants.FellowshipSalary.RegistrationPeriod();
-const payoutPeriod =
-  await fellowshipApi.constants.FellowshipSalary.PayoutPeriod();
+    // get the value for an account
+    const salaryStatus =
+        await fellowshipApi.query.FellowshipSalary.Status.getValue();
 
-for (let cycle = 0; cycle < 12; cycle++) {
-  calendar.createEvent({
-    allDay: true,
-    start: blockToDate(currentCycleStart),
-    end: blockToDate(currentCycleStart + registrationPeriod),
-    summary: "Salary Registration",
-  });
+    if (salaryStatus == undefined) {
+        return res.json({});
+    }
 
-  calendar.createEvent({
-    allDay: true,
-    start: blockToDate(currentCycleStart + registrationPeriod),
-    end: blockToDate(currentCycleStart + registrationPeriod + payoutPeriod),
-    summary: "Payout Registration",
-  });
+    let currentCycleStart = salaryStatus.cycle_start;
+    const registrationPeriod =
+        await fellowshipApi.constants.FellowshipSalary.RegistrationPeriod();
+    const payoutPeriod =
+        await fellowshipApi.constants.FellowshipSalary.PayoutPeriod();
 
-  currentCycleStart += registrationPeriod + payoutPeriod;
-}
+    for (let cycle = 0; cycle < 12; cycle++) {
+        calendar.createEvent({
+            allDay: true,
+            start: blockToDate(currentCycleStart),
+            end: blockToDate(currentCycleStart + registrationPeriod),
+            summary: "Salary Registration",
+        });
 
-const ADDRESS = "13fvj4bNfrTo8oW6U8525soRp6vhjAFLum6XBdtqq9yP22E7";
-const [coreParams, memberStatus, memberRank] = await Promise.all([
-  fellowshipApi.query.FellowshipCore.Params.getValue(),
-  fellowshipApi.query.FellowshipCore.Member.getValue(ADDRESS),
-  fellowshipApi.query.FellowshipCollective.Members.getValue(ADDRESS),
-]);
+        calendar.createEvent({
+            allDay: true,
+            start: blockToDate(currentCycleStart + registrationPeriod),
+            end: blockToDate(
+                currentCycleStart + registrationPeriod + payoutPeriod,
+            ),
+            summary: "Payout Registration",
+        });
 
-if (!memberRank || !memberStatus)
-  throw new Error(`Member ${ADDRESS} not found`);
+        currentCycleStart += registrationPeriod + payoutPeriod;
+    }
 
-let timeout: number;
-let approachSummary: string;
-let happenedSummary: string;
+    const [coreParams, memberStatus, memberRank] = await Promise.all([
+        fellowshipApi.query.FellowshipCore.Params.getValue(),
+        fellowshipApi.query.FellowshipCore.Member.getValue(
+            "13fvj4bNfrTo8oW6U8525soRp6vhjAFLum6XBdtqq9yP22E7",
+        ),
+        fellowshipApi.query.FellowshipCollective.Members.getValue(
+            "13fvj4bNfrTo8oW6U8525soRp6vhjAFLum6XBdtqq9yP22E7",
+        ),
+    ]);
 
-if (memberRank == 0) {
-  timeout = coreParams.offboard_timeout;
-  approachSummary = "Offboarding approaching";
-  happenedSummary = "Offboarded?";
-} else {
-  timeout = coreParams.demotion_period[memberRank - 1];
-  approachSummary = "Demotion approaching";
-  happenedSummary = "Demoted?";
-}
+    if (memberStatus == undefined || memberRank == undefined) {
+        return res.json({});
+    }
 
-let offboardDate = blockToDate(memberStatus.last_proof + timeout);
-let offboardEventStart = new Date(offboardDate);
-offboardEventStart.setDate(offboardEventStart.getDate() - 7 * 4);
+    let timeout;
+    let approachSummary;
+    let happenedSummary;
 
-calendar.createEvent({
-  allDay: true,
-  start: offboardEventStart,
-  end: offboardDate,
-  summary: approachSummary,
-});
+    if (memberRank == 0) {
+        timeout = coreParams.offboard_timeout;
+        approachSummary = "Offboarding approaching";
+        happenedSummary = "Offboarded?";
+    } else {
+        timeout = coreParams.demotion_period[memberRank - 1];
+        approachSummary = "Demotion approaching";
+        happenedSummary = "Demoted?";
+    }
 
-let oneDayAfter = new Date(offboardDate);
-oneDayAfter.setDate(offboardDate.getDate() + 1);
+    let offboardDate = blockToDate(memberStatus.last_proof + timeout);
+    let offboardEventStart = new Date(offboardDate);
+    offboardEventStart.setDate(offboardEventStart.getDate() - 7 * 4);
 
-calendar.createEvent({
-  allDay: true,
-  start: offboardDate,
-  end: oneDayAfter,
-  summary: happenedSummary,
-});
+    calendar.createEvent({
+        allDay: true,
+        start: offboardEventStart,
+        end: offboardDate,
+        summary: approachSummary,
+    });
 
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+    let oneDayAfter = new Date(offboardDate);
+    oneDayAfter.setDate(offboardDate.getDate() + 1);
+
+    calendar.createEvent({
+        allDay: true,
+        start: offboardDate,
+        end: oneDayAfter,
+        summary: happenedSummary,
+    });
+
+    let response = res.send(calendar.toString());
+    response.setHeader("Content-Type", "text/calendar; charset=utf-8");
+    response.setHeader(
+        "Content-Disposition",
+        'attachment; filename="calendar.ics"',
+    );
+
+    return response;
 }
